fa494c6053c7c367b7449eb28f89c544cfa29995
Shovan Mishra <86484039+smb-220@users.noreply.github.com>
2023-09-06 00:11:19 +0530

    Problem of the Day(06-09-2023).cpp
    The solution to Problem of the Day (06-09-2023) on GFG using C++

---
+++ Problem of the Day(06-09-2023).cpp
@@ -0,0 +1,34 @@
+class Solution 
+{
+    public:
+	void dfs(int u, vector<int>adj[], vector<bool>&vis)
+	{
+		vis[u] = true;
+		
+		for(auto v: adj[u])
+		{
+			if(!vis[v])
+				dfs(v, adj, vis);
+		}
+	}
+	
+	int findMotherVertex(int V, vector<int>adj[])
+	{
+		vector<bool>vis(V, false);
+		int v;
+		for(int i = 0; i < V; i++)
+		{
+			if(!vis[i]){
+				dfs(i, adj, vis);
+				v = i;
+			}
+		}
+		vis.assign(V, false);
+		dfs(v, adj, vis);
+	
+		for(auto i: vis)
+			if(!i)
+				return -1;			
+		return v;
+	}
+};
